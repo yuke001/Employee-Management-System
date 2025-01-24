a2c03b804281134a32af18ca0756f43eb9b49499
yuke001 <yukenthirans2018@gmail.com>
2025-01-24 19:59:54 +0530

    creating ems project

---
+++ src/main/java/com/yuke/ems/EmsApplication.java
@@ -0,0 +1,15 @@
+package com.yuke.ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsApplication.class, args);
+		System.err.println("server running");
+		
+	}
+
+}
