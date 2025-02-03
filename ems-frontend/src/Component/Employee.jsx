75e9ce82cdd676e86f89fda82b11acd7ffa33e0e
yuke001 <yukenthirans2018@gmail.com>
2025-02-03 13:02:51 +0530

    update

--- ems-frontend/src/Component/Employee.jsx
+++ ems-frontend/src/Component/Employee.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { createEmployee,getEmployee } from '../Services/EmployeeService';
+import { createEmployee, getEmployee } from '../Services/EmployeeService';
 import { useNavigate, useParams } from 'react-router-dom';
 
 
@@ -11,7 +11,7 @@ const Employee = () => {
 
   const { id } = useParams();
 
-  const navigate = useNavigate();
+
 
   const [error, setError] = useState({
     firstName: '',
@@ -19,163 +19,174 @@ const Employee = () => {
     email: ''
   });
 
+  const navigate = useNavigate();
+
+    useEffect(()=>{
+      if(id){
+        getEmployee(id).then((response)=>{
+          setFirstName(response.data.firstName);
+          setLastName(response.data.lastName);
+          setEmail(response.data.email);
+        }).catch(error=>{
+          console.error(error);
+        })
+      }
+    },[id])
+
+
+
   useEffect(() => {
-    if(id){
-      getEmployee(id).then((response)=>{
+    if (id) {
+      getEmployee(id).then((response) => {
 
         setFirstName(response.data.firstName);
         setLastName(response.data.lastName);
         setEmail(response.data.email);
-      }).catch(error =>{
+      }).catch(error => {
         console.error(error);
       })
     }
-  
+
   }, [id]);
+
+
+
+
+
+  //saveEmployee
+  function saveEmployee(e) {
+    e.preventDefault();
+
+    if (validateForm()) {
+
+      const employee = { firstName, lastName, email };
+      console.log(employee);
+
+      // create Employee
+      createEmployee(employee).then((response) => {
+        console.log(response.data);
+        navigate('/employees');
+      }).catch(error => {
+        console.log(error);
+      })
+    }
+
+  }
+
   
-  
 
-    // // handle first name 
-    // const handleFirstName = (e) => setFirstName(e.target.value);
+  // form validation
+  function validateForm() {
+    let valid = true;
 
-    // // handle last name
-    // const handleLastName = (e) => setLastName(e.target.value);
-    // // handle email
-    // const handleEmail = (e) => setEmail(e.target.value);
+    const errorsCopy = { ...error }
 
-    //saveEmployee
-    function saveEmployee(e) {
-      e.preventDefault();
-
-      if (validateForm()) {
-
-        const employee = { firstName, lastName, email };
-        console.log(employee);
-
-        // create Employee
-        createEmployee(employee).then((response) => {
-          console.log(response.data);
-          navigate('/employees');
-        }).catch(error => {
-          console.log(error);
-        })
-      }
-
+    if (firstName.trim()) {
+      errorsCopy.firstName = '';
+    }
+    else {
+      errorsCopy.firstName = 'First Name is required';
+      valid = false;
     }
 
-    // function  for page title change
-
-    function pageTitle() {
-      if (id) {
-        return <h2 className='text-center'>Update Employee</h2>
-      } else {
-        return <h2 className='text-center'>Add Employee</h2>
-      }
+    if (lastName.trim()) {
+      errorsCopy.lastName = '';
+    }
+    else {
+      errorsCopy.lastName = 'Last Name is required';
+      valid = false;
     }
 
-    // form validation
-    function validateForm() {
-      let valid = true;
-
-      const errorsCopy = { ...error }
-
-      if (firstName.trim()) {
-        errorsCopy.firstName = '';
-      }
-      else {
-        errorsCopy.firstName = 'First Name is required';
-        valid = false;
-      }
-
-      if (lastName.trim()) {
-        errorsCopy.lastName = '';
-      }
-      else {
-        errorsCopy.lastName = 'Last Name is required';
-        valid = false;
-      }
-
-      if (email.trim()) {
-        errorsCopy.email = '';
-      }
-      else {
-        errorsCopy.email = 'Email is required';
-        valid = false;
-      }
-
-      setError(errorsCopy);
-      return valid;
+    if (email.trim()) {
+      errorsCopy.email = '';
+    }
+    else {
+      errorsCopy.email = 'Email is required';
+      valid = false;
     }
 
+    setError(errorsCopy);
+    return valid;
+  }
+
+// function  for page title change
+
+function pageTitle() {
+  if (id) {
+    return <h2 className='text-center'>Update Employee</h2>
+  } else {
+    return <h2 className='text-center'>Add Employee</h2>
+  }
+}
 
 
 
-    return (
-      <div>
-        <div className='container'>
-          <br /><br />
-          <div className='row'>
-            <div className='card col-md-6 offset-md-3 offset-md-3'>
-              {/* <h2 className='text-center'>Add Employee</h2> */}
-              {
-                pageTitle()
-              }
-              <div className='card-body'>
+  return (
+    <div>
+      <div className='container'>
+        <br /><br />
+        <div className='row'>
+          <div className='card col-md-6 offset-md-3 offset-md-3'>
+            {/* <h2 className='text-center'>Add Employee</h2> */}
+            {
+              pageTitle()
+            }
+            <div className='card-body'>
 
-                <form action="">
+              <form action="">
 
-                  {/* First Name */}
-                  <div className='form-group mb-2'>
-                    <label className='form-label'>First Name :</label>
-                    <input
-                      type="text"
-                      placeholder='Enter Employee First Name'
-                      name='firstName'
-                      value={firstName}
-                      className={`form-control ${error.firstName ? 'is-invalid' : ''} `}
-                      onChange={(e) => setFirstName(e.target.value)} />
+                {/* First Name */}
+                <div className='form-group mb-2'>
+                  <label className='form-label'>First Name :</label>
+                  <input
+                    type="text"
+                    placeholder='Enter Employee First Name'
+                    name='firstName'
+                    value={firstName}
+                    className={`form-control ${error.firstName ? 'is-invalid' : ''} `}
+                    onChange={(e) => setFirstName(e.target.value)} />
 
-                    {error.firstName && <div className='invalid-feedback'>{error.firstName}</div>}
-                  </div>
+                  {error.firstName && <div className='invalid-feedback'>{error.firstName}</div>}
+                </div>
 
-                  {/* Last Name */}
-                  <div className='form-group mb-2'>
-                    <label className='form-label'>Last Name :</label>
-                    <input
-                      type="text"
-                      placeholder='Enter Employee Last Name'
-                      name='lastName'
-                      value={lastName}
-                      className={`form-control ${error.lastName ? 'is-invalid' : ''} `}
-                      onChange={(e) => setLastName(e.target.value)} />
+                {/* Last Name */}
+                <div className='form-group mb-2'>
+                  <label className='form-label'>Last Name :</label>
+                  <input
+                    type="text"
+                    placeholder='Enter Employee Last Name'
+                    name='lastName'
+                    value={lastName}
+                    className={`form-control ${error.lastName ? 'is-invalid' : ''} `}
+                    onChange={(e) => setLastName(e.target.value)} />
 
-                    {error.lastName && <div className='invalid-feedback'>{error.lastName}</div>}
-                  </div>
+                  {error.lastName && <div className='invalid-feedback'>{error.lastName}</div>}
+                </div>
 
-                  {/* Email */}
-                  <div className='form-group mb-2'>
-                    <label className='form-label'>Email :</label>
-                    <input
-                      type="email"
-                      placeholder='Enter Employee Email'
-                      name='email'
-                      value={email}
-                      className={`form-control ${error.email ? 'is-invalid' : ''} `}
-                      onChange={(e) => setEmail(e.target.value)} />
-                    {error.email && <div className='invalid-feedback'>{error.email}</div>}
-                  </div>
+                {/* Email */}
+                <div className='form-group mb-2'>
+                  <label className='form-label'>Email :</label>
+                  <input
+                    type="email"
+                    placeholder='Enter Employee Email'
+                    name='email'
+                    value={email}
+                    className={`form-control ${error.email ? 'is-invalid' : ''} `}
+                    onChange={(e) => setEmail(e.target.value)} />
+                  {error.email && <div className='invalid-feedback'>{error.email}</div>}
+                </div>
 
-                  {/* Button */}
+                {/* Button */}
 
-                  <button className='btn btn-success' onClick={saveEmployee}>Submit</button>
+                <button className='btn btn-success' onClick={saveEmployee}>Submit</button>
 
-                </form>
-              </div>
+              </form>
             </div>
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
+}
 
-  export default Employee
+export default Employee
--- ems-frontend/src/Services/EmployeeService.js
+++ ems-frontend/src/Services/EmployeeService.js
@@ -4,14 +4,9 @@ const REST_API_BASE_URL = "http://localhost:9000/api/employees";
 
 export const listEmployees = () => axios.get(REST_API_BASE_URL); // get all employees
 
-export const createEmployee = (employee) =>
-  axios.post(REST_API_BASE_URL, employee); // create employee
+export const createEmployee = (employee) =>axios.post(REST_API_BASE_URL, [employee]); // create employee
 
-// export const getEmployee = (employeeId) =>
-//   axios.get(REST_API_BASE_URL + "/" + employeeId); // get employee by id
-
-export const getEmployee = (employeeId) =>
-    axios.get(`${REST_API_BASE_URL}/id/${employeeId}`);
+export const getEmployee = (employeeId) => axios.get(REST_API_BASE_URL +'/'+employeeId)
 
 
 listEmployees().then((response) => console.log(response.data));
